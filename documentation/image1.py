0f3456abdb7c053e5d79a8f6ac4e002d6473b4ef
Eli Heuer <elih@protonmail.com>
2024-02-11 23:40:13 -0800

    Update image1

--- documentation/image1.py
+++ documentation/image1.py
@@ -4,29 +4,30 @@
 # $ cd my-font
 # $ python3 documentation/image1.py --output documentation/image1.png
 
-# Import moduels from external python packages: https://pypi.org/
-from drawbot_skia.drawbot import *
-from fontTools.ttLib import TTFont
-from fontTools.misc.fixedTools import floatToFixedToStr
+import argparse
 
 # Import moduels from the Python Standard Library: https://docs.python.org/3/library/
 import subprocess
 import sys
-import argparse
+
+# Import moduels from external python packages: https://pypi.org/
+from drawbot_skia.drawbot import *
+from fontTools.misc.fixedTools import floatToFixedToStr
+from fontTools.ttLib import TTFont
 
 # Constants, these are the main "settings" for the image
-WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/ttf/Rubik-Regular.ttf"
+WIDTH, HEIGHT, MARGIN, FRAMES = 4096, 2048, 256, 1
+FONT_PATH = "fonts/FGGrotesk-Regular.ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 
-BIG_TEXT = "AaBb"
-BIG_TEXT_FONT_SIZE = 730
+BIG_TEXT = "AFont.Garden™"
 BIG_TEXT_SIDE_MARGIN = MARGIN * 1
-BIG_TEXT_BOTTOM_MARGIN = MARGIN * 2
+BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5
 
-GRID_VIEW = False # Toggle this for a grid overlay
+GRID_VIEW = True  # Toggle this for a grid overlay
+GRID_VIEW = False  # Toggle this for a grid overlay
 
 # Handel the "--output" flag
 # For example: $ python3 documentation/image1.py --output documentation/image1.png
@@ -53,7 +54,7 @@ def grid():
     STEP_X, STEP_Y = 0, 0
     INCREMENT_X, INCREMENT_Y = MARGIN / 2, MARGIN / 2
     rect(MARGIN, MARGIN, WIDTH - (MARGIN * 2), HEIGHT - (MARGIN * 2))
-    for x in range(29):
+    for x in range(61):
         polygon((MARGIN + STEP_X, MARGIN), (MARGIN + STEP_X, HEIGHT - MARGIN))
         STEP_X += INCREMENT_X
     for y in range(29):
@@ -89,48 +90,32 @@ def draw_main_text():
     fill(1)
     stroke(None)
     font(FONT_PATH)
-    fontSize(BIG_TEXT_FONT_SIZE)
-    # Adjust this line to center main text manually.
-    # TODO: This should be done automatically when drawbot-skia
-    # has support for textBox() and FormattedString
-    #text(BIG_TEXT, ((WIDTH / 2) - MARGIN * 4.75, (HEIGHT / 2) - MARGIN * 2.5))
+
+    fontSize(547)
     text(BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN))
 
+    fontSize(410)
+    # text(BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * 1.5)))
 
-# Divider lines
-def draw_divider_lines():
-    stroke(1)
-    strokeWidth(5)
-    lineCap("round")
-    line((MARGIN, HEIGHT - (MARGIN * 1.5)), (WIDTH - MARGIN, HEIGHT - (MARGIN * 1.5)))
-    line((MARGIN, MARGIN + (MARGIN / 2)), (WIDTH - MARGIN, MARGIN + (MARGIN / 2)))
-    stroke(None)
+    fontSize(280)
+    # text(BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * 2.5)))
 
+    fontSize(230)
+    text(BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * 3.5)))
 
-# Draw text describing the font and it's git status & repo URL
-def draw_auxiliary_text():
-    # Setup
-    font(AUXILIARY_FONT)
-    fontSize(AUXILIARY_FONT_SIZE)
-    POS_TOP_LEFT = (MARGIN, HEIGHT - MARGIN * 1.25)
-    POS_TOP_RIGHT = (WIDTH - MARGIN, HEIGHT - MARGIN * 1.25)
-    POS_BOTTOM_LEFT = (MARGIN, MARGIN)
-    POS_BOTTOM_RIGHT = (WIDTH - MARGIN * 0.95, MARGIN)
-    URL_AND_HASH = MY_URL + "at commit " + MY_HASH
-    URL_AND_HASH = URL_AND_HASH.replace("\n", " ")
-    # Draw Text
-    text(FONT_NAME, POS_TOP_LEFT, align="left")
-    text(FONT_VERSION, POS_TOP_RIGHT, align="right")
-    text(URL_AND_HASH, POS_BOTTOM_LEFT, align="left")
-    text(FONT_LICENSE, POS_BOTTOM_RIGHT, align="right")
+    fontSize(60)
+    # text(BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * 3.25)))
+
+    fontSize(50)
+    text(
+        BIG_TEXT, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * 4))
+    )  # 3.375
 
 
 # Build and save the image
 if __name__ == "__main__":
     draw_background()
     draw_main_text()
-    draw_divider_lines()
-    draw_auxiliary_text()
     # Save output, using the "--output" flag location
     saveImage(args.output)
     # Print done in the terminal
