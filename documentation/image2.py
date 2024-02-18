b57784596d390b4685677fb7d23cea498be9a682
Eli Heuer <elih@protonmail.com>
2024-02-17 19:32:06 -0800

    Add image2.png

--- documentation/image2.py
+++ documentation/image2.py
@@ -4,32 +4,31 @@
 # $ cd my-font
 # $ python3 documentation/image1.py --output documentation/image1.png
 
-# Import moduels from external python packages: https://pypi.org/
-from drawbot_skia.drawbot import *
-from fontTools.ttLib import TTFont
-from fontTools.misc.fixedTools import floatToFixedToStr
+import argparse
 
 # Import moduels from the Python Standard Library: https://docs.python.org/3/library/
 import subprocess
 import sys
-import argparse
+
+# Import moduels from external python packages: https://pypi.org/
+from drawbot_skia.drawbot import *
+from fontTools.misc.fixedTools import floatToFixedToStr
+from fontTools.ttLib import TTFont
 
 # Constants, these are the main "settings" for the image
-WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/ttf/Rubik-Regular.ttf"
+WIDTH, HEIGHT, MARGIN, FRAMES = 4096, 4096, 512, 1
+FONT_PATH = "fonts/FGGrotesk-Regular.ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 
-LINE_ONE = "ABCDEFGHIJKLMNOPQ"
-LINE_TWO = "RSTUVWXYZ123456789"
-LINE_THREE = "abcdefghijklmnopqrstu"
-LINE_FOUR = "vwxyz,.;:!@#$%^&*(){}[]"
-BIG_TEXT_FONT_SIZE = 160
+BIG_TEXT = "AFont.Garden™"
+BIG_TEXT = "Font.Garden"
 BIG_TEXT_SIDE_MARGIN = MARGIN * 1
-BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.45
+BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5
 
-GRID_VIEW = False # Toggle this for a grid overlay
+GRID_VIEW = True  # Toggle this for a grid overlay
+#GRID_VIEW = False  # Toggle this for a grid overlay
 
 # Handel the "--output" flag
 # For example: $ python3 documentation/image1.py --output documentation/image1.png
@@ -56,7 +55,7 @@ def grid():
     STEP_X, STEP_Y = 0, 0
     INCREMENT_X, INCREMENT_Y = MARGIN / 2, MARGIN / 2
     rect(MARGIN, MARGIN, WIDTH - (MARGIN * 2), HEIGHT - (MARGIN * 2))
-    for x in range(29):
+    for x in range(61):
         polygon((MARGIN + STEP_X, MARGIN), (MARGIN + STEP_X, HEIGHT - MARGIN))
         STEP_X += INCREMENT_X
     for y in range(29):
@@ -64,6 +63,24 @@ def grid():
         STEP_Y += INCREMENT_Y
     polygon((WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
     polygon((0, HEIGHT / 2), (WIDTH, HEIGHT / 2))
+
+
+# Draws a display grid
+def display_grid():
+    fill(None)
+    stroke(1, 1, 1, 1)
+    strokeWidth(4)
+    STEP_X, STEP_Y = 0, 0
+    INCREMENT_X, INCREMENT_Y = MARGIN / 2, MARGIN / 2
+    rect(MARGIN, MARGIN, WIDTH - (MARGIN * 2), HEIGHT - (MARGIN * 2))
+    for x in range(29):
+        polygon((MARGIN + STEP_X, MARGIN), (MARGIN + STEP_X, HEIGHT - MARGIN))
+        STEP_X += INCREMENT_X
+    for y in range(29):
+        polygon((MARGIN, MARGIN + STEP_Y), (WIDTH - MARGIN, MARGIN + STEP_Y))
+        STEP_Y += INCREMENT_Y
+    #polygon((WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
+    #polygon((0, HEIGHT / 2), (WIDTH, HEIGHT / 2))
 
 
 # Remap input range to VF axis range
@@ -92,53 +109,18 @@ def draw_main_text():
     fill(1)
     stroke(None)
     font(FONT_PATH)
-    fontSize(BIG_TEXT_FONT_SIZE)
-    # Adjust this line to center main text manually.
-    # TODO: This should be done automatically when drawbot-skia
-    # has support for textBox() and FormattedString
-    LEADING = 1.2
-    text(LINE_ONE, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN))
-    text(LINE_TWO, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * LEADING)))
-    text(LINE_THREE, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * (LEADING * 2))))
-    text(LINE_FOUR, (BIG_TEXT_SIDE_MARGIN, BIG_TEXT_BOTTOM_MARGIN - (MARGIN * (LEADING * 3))))
 
-
-# Divider lines
-def draw_divider_lines():
-    stroke(1)
-    strokeWidth(5)
-    lineCap("round")
-    line((MARGIN, HEIGHT - (MARGIN * 1.5)), (WIDTH - MARGIN, HEIGHT - (MARGIN * 1.5)))
-    line((MARGIN, MARGIN + (MARGIN / 2)), (WIDTH - MARGIN, MARGIN + (MARGIN / 2)))
-    stroke(None)
-
-
-# Draw text describing the font and it's git status & repo URL
-def draw_auxiliary_text():
-    # Setup
-    font(AUXILIARY_FONT)
-    fontSize(AUXILIARY_FONT_SIZE)
-    POS_TOP_LEFT = (MARGIN, HEIGHT - MARGIN * 1.25)
-    POS_TOP_RIGHT = (WIDTH - MARGIN, HEIGHT - MARGIN * 1.25)
-    POS_BOTTOM_LEFT = (MARGIN, MARGIN)
-    POS_BOTTOM_RIGHT = (WIDTH - MARGIN * 0.95, MARGIN)
-    #URL_AND_HASH = "github.com/googlefonts/googlefonts-project-template " + "at commit " + MY_HASH
-    URL_AND_HASH = MY_URL + "at commit " + MY_HASH
-    URL_AND_HASH = URL_AND_HASH.replace("\n", " ")
-    # Draw Text
-    #text("Your Font Regular", POS_TOP_LEFT, align="left")
-    text(FONT_NAME, POS_TOP_LEFT, align="left")
-    text(FONT_VERSION, POS_TOP_RIGHT, align="right")
-    text(URL_AND_HASH, POS_BOTTOM_LEFT, align="left")
-    text(FONT_LICENSE, POS_BOTTOM_RIGHT, align="right")
-
+    fontSize(547)
+    fontSize(564)
+    fontSize(560)
+    fontSize(562)
+    text("Font.Garden", (1*MARGIN, 6*MARGIN))
 
 # Build and save the image
 if __name__ == "__main__":
     draw_background()
     draw_main_text()
-    draw_divider_lines()
-    draw_auxiliary_text()
+    display_grid()
     # Save output, using the "--output" flag location
     saveImage(args.output)
     # Print done in the terminal
